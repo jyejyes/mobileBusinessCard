62bf54e6fedec8e0226039fa065db181f87e9981
jyejyes <jh100m1@jellypage.com>
2023-08-27 00:24:46 +0900

    사파리에서 프리텐다드 안먹는 문제

--- src/styles/reset.tsx
+++ src/styles/reset.tsx
@@ -1,6 +1,14 @@
 import { css } from "@emotion/react";
 
 const reset = css`
+  html, body {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+  }
+  
+  
   html,
   body,
   div,
@@ -90,7 +98,7 @@ const reset = css`
 
     font-size: 62.5%;
     font-weight: normal;
-    font-family: "Pretendard-Regular";
+    font-family: "Pretendard-Regular", sans-serif;
   }
 
   article,
