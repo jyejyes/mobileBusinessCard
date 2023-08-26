7d542e358685cc2c655f50154c12ece6f987dccd
박지혜 <41052126+jyejyes@users.noreply.github.com>
2023-08-26 17:32:09 +0900

    스타일 파일 추가

---
+++ src/styles/Common.ts
@@ -0,0 +1,16 @@
+export const Common = {
+  colors: [
+    { main: "#a8edea", sub: "#fed6e3", text: "#CD6381", cardBg: "#FCF5F5" },
+    { main: "#fddb92", sub: "#d1fdff", text: "#109886", cardBg: "#FAF8ED" },
+    { main: "#feada6", sub: "#f5efef", text: "#D36F66", cardBg: "#F9ECE7" },
+    { main: "#c1dfc4", sub: "#deecdd", text: "#397C4C", cardBg: "#FCFAEB" },
+    { main: "#fff1eb", sub: "#ace0f9", text: "#377BC3", cardBg: "#FEFEFE" },
+    { main: "#a1c4fd", sub: "#c2e9fb", text: "#377BC3", cardBg: "#E8F5FB" },
+    { main: "#e9defa", sub: "#cd9cf2", text: "#8B32A7", cardBg: "#FAEAFC" },
+  ],
+
+  mediaQuery: {
+    mobile: "@media (max-width: 450px)",
+    bigMobile: "@media (max-width: 600px)",
+  },
+};
